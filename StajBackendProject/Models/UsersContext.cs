133168870849fdd33e6716ed935aa1acbf760601
borakisalar <borakisalar@gmail.com>
2026-07-13 15:35:24 +0300

    Add project files.

---
+++ StajBackendProject/Models/UsersContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StajBackendProject.Models
+{
+    public class UsersContext:DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Users> Users { get; set; } = null;
+    }
+}
