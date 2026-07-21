ea54fb6b761a27c717ffada87f9b62c7d5801d75
borakisalar <borakisalar@gmail.com>
2026-07-21 14:49:26 +0300

    Add database constraints to user table

--- StajBackendProject/Models/UsersContext.cs
+++ StajBackendProject/Models/UsersContext.cs
@@ -2,14 +2,32 @@
 
 namespace StajBackendProject.Models
 {
-    public class UsersContext:DbContext
+    public class UsersContext : DbContext
     {
-        public UsersContext(DbContextOptions<UsersContext> options)
-            :base(options)
-        {
-
-        }
-
+        public UsersContext(DbContextOptions<UsersContext> options):base(options) { }
         public DbSet<Users> Users { get; set; } = null;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Users>()
+                .Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Users>()
+                .Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Users>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Users>()
+                .HasIndex(u => u.PhoneNumber)
+                .IsUnique()
+                .HasFilter("[PhoneNumber] IS NOT NULL AND [PhoneNumber] <> ''");
+        }
     }
 }
